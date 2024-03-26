425b4cf2ae93fce8dacdecaeb7af1bace5472d67
Abd-elrahman10 <162298117+Abd-elrahman10@users.noreply.github.com>
2024-03-26 16:24:37 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -44,70 +44,166 @@ void merge(Image& image1,Image& image2){
 }
 
 
+void Black_White(const string& filename) {
+    // Load the image
+    Image image(filename);
+
+    // Convert image to black and white
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = 0; j < image.height; ++j) {
+            // Calculate luminance of each pixel
+            int luminance = 0;
+            for (int k = 0; k < image.channels; ++k) {
+                luminance += image(i, j, k);
+            }
+            luminance /= image.channels;
+
+            // Set pixel to black or white based on luminance
+            for (int k = 0; k < image.channels; ++k) {
+                if (luminance <= 127) {
+                    image(i, j, k) = 0;
+                } else {
+                    image(i, j, k) = 255;
+                }
+            }
+        }
+
+    }
+
+
+    if(image.saveImage("output.jpg"))
+        cout << "Image saved successfully at the file \"output.jpg\"." << endl;
+}
+
+
+void horizontal(const string& filename) {
+    // Load the image
+    Image image(filename);
+
+    // Iterate over each row
+    for (int j = 0; j < image.height; ++j) {
+        // Iterate over the left half of the width
+        for (int i = 0; i < image.width/2; ++i) {
+            // Swap pixels from left to right
+            for (int k = 0; k < image.channels; ++k) {
+                swap(image(i, j, k), image(image.width - 1 - i, j, k));
+            }
+        }
+    }
+
+    // Save the modified image
+    image.saveImage("output.jpg");
+}
+
+
+
+void vertical(const string& filename) {
+    // Load the image
+    Image image(filename);
+
+    // Iterate over each column
+    for (int i = 0; i < image.width; ++i) {
+        // Iterate over the top half of the height
+        for (int j = 0; j < image.height / 2; ++j) {
+            // Swap pixels from top to bottom
+            for (int k = 0; k < image.channels; ++k) {
+                swap(image(i, j, k), image(i, image.height - 1 - j, k));
+            }
+        }
+    }
+
+    // Save the modified image
+    image.saveImage("output.jpg");
+
+}
+
+
 
 int main() {
-    Image image1, image2;
-    string filename,filename2;
+    Image image,image1, image2;
+    string filename, filename2;
     cout << " ==Welcome to Baby Photoshop program== " << endl;
     while (true) {
         cout << "Please choose an option : " << endl;
         cout << "1) Load New Image" << endl;
         cout << "2) Save and exit" << endl;
 
-        string option,option2,option3;
+        string option;
         cin >> option;
-        if ( option != "1" && option != "2"){
-            cout << "Invalid choice." << endl;
-            continue;
-        }
-
-        while (true) {
-            if (option == "1") {
-                cout << "Please enter the image filename you want to edit : " << endl;
-                cin >> filename;
-                try {
-                    if (image1.loadNewImage(filename)) {
-                        cout << "Image loaded successfully." << endl;
-                    }
+        if (option == "1") {
+            cout << "Please enter the image filename you want to edit : " << endl;
+            cin >> filename;
+            try {
+                if (image1.loadNewImage(filename)) {
+                    cout << "Image loaded successfully." << endl;
                 }
-                catch (...) {
-                    cout << "Error, this image file is not exist, please try again" << endl;
-                    continue;
-                }
-                while (true) {
-                    cout << "Choose the filter you want to apply :" << endl;
-                    cout << "1) Gray scale\n2) Merging images" << endl;
-                    cin >> option2;
+            }
+            catch (...) {
+                cout << "Error, this image file does not exist, please try again" << endl;
+                continue;
+            }
 
+            while (true) {
+                cout << "Choose the filter you want to apply :" << endl;
+                cout << "1) Gray scale\n2) Merging images\n3) Black and White\n4) Flip Image" << endl;
+                string option2;
+                cin >> option2;
 
-                    if (option2 == "1") {
-                        grayScale(image1);
-                        break;
-                    } else if (option2 == "2") {
-                        while (true) {
-                            cout << "Enter the name of the second image you want to merge with :  " << endl;
-                            cin >> filename2;
-                            try {
-                                image2.loadNewImage(filename2);
+                if (option2 == "1") {
+                    grayScale(image1);
+                    break;
+                } else if (option2 == "2") {
+                    while (true) {
+                        cout << "Enter the name of the second image you want to merge with :  " << endl;
+                        cin >> filename2;
+                        try {
+                            if (image2.loadNewImage(filename2)) {
                                 merge(image1, image2);
                                 break;
                             }
-                            catch (...) {
-                                cout << "Error, this image file is not exist, please try again" << endl;
-                                continue;
-                            }
                         }
-                        break;
+                        catch (...) {
+                            cout << "Error, this image file does not exist, please try again" << endl;
+                            continue;
+                        }
                     }
+                    break;
+                } else if (option2 == "3") {
+                    Black_White(filename);
+                    break;
                 }
-                if (option == "2") {
-                    cout << "Exiting" << endl;
-                    return 0;
-                } else {
+                else if(option2=="4") {
+                    int choice;
+                    do {
+                        cout << "You need to flip: \n1) Horizontally\n2) Vertically\n";
+
+                        cin >> choice;
+
+                        if (choice == 1) {
+                            horizontal(filename);
+                            cout << "Image saved successfully at the file \"output.jpg\"." << "'\n";
+                            break;
+                        } else if (choice == 2) {
+                            vertical(filename);
+                            cout << "Image saved successfully at the file \"output.jpg\"." << "'\n";
+                            break;
+                        } else {
+                            cout << "Enter a valid number (1 or 2)\n";
+                            break;
+                        }
+                    } while (choice!=1 || choice!=2);
+                }
+                else {
                     cout << "Invalid choice." << endl;
-                    continue;
                 }
+                break;
             }
+        } else if (option == "2") {
+            cout << "Exiting" << endl;
+            break; // Exit the loop
+        } else {
+            cout << "Invalid choice." << endl;
         }
     }
+    return 0;
 }
